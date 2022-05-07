b29141f2b150b3b6f775a001c53a40a5f03141b6
AleGV258 <ale.gv258@gmail.com>
2022-05-06 23:17:27 -0500

    weekly_mission_4

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,17 +1,17 @@
 // GitHub: @AleGV258
 
-const Reader = require('../../lib/utils/Reader')
+const Reader = require("../../lib/utils/Reader");
 
 describe("Unit Tests for Reader class", () => {
-    test('Create Reader object', () => {
-        const reader = new Reader()
-        expect(reader).toEqual({})
-    })
+    test("Create Reader object", () => {
+        const reader = new Reader();
+        expect(reader).toEqual({});
+    });
 
-    test('Create Reader object', () => {
-        const reader = new Reader()
-        expect(reader).toEqual({})
-    })
-})
+    test("Create Reader object", () => {
+        const reader = new Reader();
+        expect(reader).toEqual({});
+    });
+});
 
 // npm test test/utils/Reader.test.js
